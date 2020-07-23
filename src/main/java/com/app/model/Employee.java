4c26b0ce5f16c2fc7a23bbbe3c1936805c2f681e
RAMCHARANTEJA <RAMCHARANTEJA@Ramchran>
2020-07-24 00:44:50 +0530

    Rc01 Model class completed

--- src/main/java/com/app/model/Employee.java
+++ src/main/java/com/app/model/Employee.java
@@ -1,11 +1,24 @@
 package com.app.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "emo_tab")
 public class Employee {
 
-  
+  @Id
+  @Column(name="emp_id_col")
   private Integer id;
   
+  @Column(name = "emp_name_col")
   private Integer empName;
   
+  @Column(name="emp_city_col")
   private Integer empCity;
 }
